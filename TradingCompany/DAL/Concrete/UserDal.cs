193a8a0257a1283527f91a23e8e575c353bd7800
KhrystynaMarusenko <marusenkokhrystyna@gmail.com>
2020-10-29 16:10:19 +0200

    reworked structure and logic of the project, added database, created Entities, created DTO, classes for each table, created DAL, created CRUD methods for all entities, created console app, worked on menu.

---
+++ TradingCompany/DAL/Concrete/UserDal.cs
@@ -0,0 +1,67 @@
+﻿using DAL.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete
+{
+    public class UserDal : ADalCrud<User> // ADalRead<User>
+    {
+        public UserDal() : base()
+        {
+        }
+
+        // class ADalRead
+        protected override string GetTableName()
+        {
+            return "[USER]";
+        }
+
+        protected override User CreateInstance(ICollection<string> args)
+        {
+            List<string> parameters = args.ToList<string>();
+            User user = new User(parameters[Convert.ToInt32(EUser.NAME)],
+                parameters[Convert.ToInt32(EUser.LOGIN)],
+                parameters[Convert.ToInt32(EUser.PASSWORD)],
+                Convert.ToInt32(parameters[Convert.ToInt32(EUser.AGE)]));
+            user.Id = Convert.ToInt32(parameters[Convert.ToInt32(EUser.ID)]);
+            return user;
+        }
+
+        // class ADalCrud
+        protected override IDictionary<string, string> GetValues(User user)
+        {
+            IDictionary<string, string> result = new Dictionary<string, string>();
+            result.Add(EUser.ID.ToString().ToLower(), user.Id.ToString());
+            result.Add(EUser.NAME.ToString().ToLower(), user.Name);
+            result.Add(EUser.LOGIN.ToString().ToLower(), user.Login);
+            result.Add(EUser.PASSWORD.ToString().ToLower(), user.HashPassword);
+            result.Add(EUser.AGE.ToString().ToLower(), user.Age.ToString());
+
+            return result;
+        }
+
+        public void PrintUser(User user)
+        {
+            Console.WriteLine(
+                   "\nID: " + user.Id.ToString() +
+                   "\nNAME: " + user.Name +
+                   "\nLOGIN: " + user.Login +
+                   "\nPASSWORD: " + user.HashPassword +
+                   "\nAGE: " + user.Age.ToString() + "\n");
+        }
+
+        public void PrintListOfUsers(IList<User> users)
+        {
+            foreach (User user in users)
+            {
+                PrintUser(user);
+            }
+        }
+
+    }
+}
---
+++ TradingCompany/DTO/ItemDTO.cs
@@ -0,0 +1,19 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ItemDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category category { get; set; }
+        public decimal Price { get; set; }
+        public string InStock { get; set; }
+    }
+}
